32f54c3c7678cf640e5b1d231e3a75a4f91f83be
combizera <ycombi349@gmail.com>
2024-08-13 16:36:33 -0300

    feat: arrays e loop exercicios

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const consoles = ['switch', 'ps', 'xbox']
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
 
-for (item = 0; item < consoles.length; item++){
-  if(consoles[item] === 'ps'){
+const years = [1959, 1962, 1970, 1994, 2002];
+console.log(years);
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+years.forEach(function(year){
+  console.log(`O brasil ganhou a copa de ${year}`)
+});
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+
+for(i = 0; i < frutas.length;){
+  console.log(i, frutas[i]);
+  if(frutas[i] === 'Pera'){
     break;
   }
-  console.log(consoles[item]);
+  i++;
 }
 
-const frutas = ["Banana", "Maça", "Pera", "Uva"];
-frutas.forEach(function (fruta, index) {
-  console.log(index, fruta);
-});
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+const lastItem = frutas.length - 1;
+console.log(frutas[lastItem]);
