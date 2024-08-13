b9b1a922439ed42feb5312ee5e5f1a91d84553b9
combizera <ycombi349@gmail.com>
2024-08-13 17:34:08 -0300

    feat: escopo exerc

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-// Some 500 ao valor de scroll abaixo,
-// atribuindo o novo valor a scroll
-var scroll = 1000;
-console.log(scroll);
-scroll += 500;
-console.log(scroll);
+// Por qual motivo o código abaixo retorna com erros?
+// {
+//   const marca = 'Fiat';
+//   let portas = 4;
+//   var cor = 'preto';
+// }
+// console.log(var, marca, portas);
 
-// Atribua true para a variável darCredito,
-// caso o cliente possua carro e casa.
-// E false caso o contrário.
-var possuiCarro = true;
-var possuiCasa = true;
+// const e let respeitam o escopo definido
 
-let darCredito = possuiCarro && possuiCasa == true ? 'tem carro' : 'ta duro fio';
+// Como corrigir o erro abaixo?
+const dois = 2;
+function somarDois(x) {
+  return x + dois;
+}
+function dividirDois(x) {
+  return x + dois;
+}
+console.log(somarDois(4));
+console.log(dividirDois(6));
+console.log('===');
 
-console.log(darCredito)
+// O que fazer para total retornar 500?
+var numero = 50;
+
+for(let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+console.log('===');
+console.log(numero);
+
+const total = 10 * numero;
+console.log(total);
