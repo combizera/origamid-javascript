f19a1edac2ff53f096907918b86020e6f9eaf652
combizera <ycombi349@gmail.com>
2024-08-15 21:58:28 -0300

    classes and attributes

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
-// Retorne no console todas as imagens do site
+const $menu = document.querySelector('.menu');
+const $animals = document.querySelector('.animais');
+
+$menu.classList.add('ativo');
+
+console.log($menu.classList, $animals.attributes);
+
+const $img = document.querySelector('img');
+
+console.log($img.getAttribute('alt'));
+$imgWithAlt = $img.setAttribute('alt', 'gatooo')
+console.log($img.getAttribute('alt'));
+
+// Adicione a classe ativo a todos os itens do menu
+const $menuItem = $menu.querySelectorAll('li');
+
+$menuItem.forEach(function(item, index){
+  item.classList.add('ativo');
+  console.log(index, item);
+});
+console.log('===')
+
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+$menuItem.forEach(function(item, index){
+  item.classList.remove('ativo');
+  console.log(index, item);
+  if(index == 0){
+    console.log(item.innerText)
+    item.classList.add('ativo')
+  }
+});
+
+// Verifique se as imagens possuem o atributo alt
 const $imgs = document.querySelectorAll('img');
-console.log($imgs);
-
-$imgs.forEach(function(item, index, array){
-  console.log(index, item);
-  console.log(array);
+$imgs.forEach(function(item, index){
+  alt = item.hasAttribute('alt')
+  if(alt){
+    console.log(item, alt);
+  } else {
+    console.log(index, item, 'Não tem alt');
+  }
 });
 
-console.log('===')
-$imgs.forEach((item, index) => {
-  console.log(index, item);
+// Modifique o href do link externo no menu
+$menuItem.forEach(function(item, index){
+  console.log(index, item)
+  $linkExterno = item.querySelector('[href^="https://"]')
+  if($linkExterno){
+    $linkExterno.setAttribute('href', 'https://combizera.com')
+    console.log($linkExterno)
+  }
 });
 
-// jeito resumido
-console.log('===')
-$imgs.forEach((item, index) => console.log(index, item));
-
-// Mostre no console cada parágrado do site
-const $p = document.querySelectorAll('p');
-$p.forEach((item, index) => console.log(index, item));
-
-// Mostre o texto dos parágrafos no console
-$p.forEach((item, index) => console.log(index, item.innerText));
-
-// Como corrigir os erros abaixo:
-$imgs.forEach((item, index) => {
-  console.log(item, index);
-});
-
-console.log('===')
-let i = 0;
-$imgs.forEach(() => {
-  console.log(i++)
-});
-
-console.log('===')
-$imgs.forEach(() => i++);
