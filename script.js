298a81bfb93db2a7ef2f28edad88b23e5288c1d1
combizera <ycombi349@gmail.com>
2024-08-17 14:37:35 -0300

    dimensions and distances exerc

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
-const $listAnimals = document.querySelector('.animais-lista');
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
+const $img = document.querySelector('img');
+console.log($img.offsetTop);
+console.log('===');
 
-console.log($listAnimals.scrollHeight);
+// Retorne a soma da largura de todas as imagens
+function somaImg(){
+  const $allImgs = document.querySelectorAll('img');
 
-const animalsTop = $listAnimals.offsetTop;
+  let soma = 0;
 
-console.log(animalsTop);
-
-const $h2 = document.querySelector('h2');
-
-const rect = $h2.getBoundingClientRect();
-
-console.log(rect);
-
-console.log(
-  window.innerWidth,
-  window.outerWidth,
-  window.scrollX,
-  window.scrollY,
-);
-
-const small = window.matchMedia('(max-width: 600px)').matches;
-
-if(small) {
-  console.log('Usuário mobile');
-} else {
-  console.log('Desktop')
+  $allImgs.forEach(function(item){
+    soma += item.offsetWidth;
+  });
+  console.log(soma);
 }
 
-console.log(small);
+window.onload = function(){
+  somaImg();
+}
+console.log('===');
+
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+const $links = document.querySelectorAll('a');
+$links.forEach(function(item){
+  // if(item)
+  itemWidth = item.offsetWidth;
+  itemHeight = item.offsetHeight;
+  if (itemWidth && itemHeight >= 48){
+    console.log(`O ${item} está dentro dos padrões`)
+  } else {
+    console.log(`O ${item} está fora dos padrões, é necessário pelo menos 48px de altura e largura e o item atual tem. Altura - ${itemHeight}. Largura - ${itemWidth}.`)
+  }
+});
+
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
+const mobile = window.matchMedia('(max-width: 720px').matches;
+if(mobile){
+  console.log(mobile);
+  const $menu = document.querySelector('.menu');
+  $menu.classList.add('menu-mobile');
+  console.log($menu);
+}
+console.log('===');
